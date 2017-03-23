18e846edb55cb61655403438cd206e3177a20d0c
ying <1798634176@qq.com>
2017-03-23 20:49:35 +0800

    商品收藏完善

--- shopCollect/js/shopCollectScd.js
+++ shopCollect/js/shopCollectScd.js
@@ -1,4 +1,8 @@
-app.controller('collectScdCtrl', ['$scope', '$http', '$stateParams','collectServ', function ($scope,$http,$stateParams,collectServ) {
+app.controller('collectScdCtrl', ['$scope', '$http', '$stateParams','collectServ', '$state',function ($scope,$http,$stateParams,collectServ,$state) {
 	$scope.data = collectServ.collectArr();
 	console.log($scope.data);
+	$scope.showShop = function () {
+		console.log(this);
+		$state.go('shop', {id:JSON.stringify(this.item)});
+	}
 }])
