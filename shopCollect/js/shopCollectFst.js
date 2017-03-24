681dc978e72a10c05de6488a6c8ee94d855cb5c3
ying <1798634176@qq.com>
2017-03-24 08:53:49 +0800

    给商品收藏添加加入购物车功能

--- shopCollect/js/shopCollectFst.js
+++ shopCollect/js/shopCollectFst.js
@@ -1,3 +1,8 @@
-app.controller('collectFstCtrl', ['$scope', '$http', 'goodsServ', function ($scope,$http,goodsServ) {
+app.controller('collectFstCtrl', ['$scope', '$http', 'goodsServ','cartServ' , function ($scope,$http,goodsServ,cartServ) {
 	$scope.data = goodsServ.goodsArr();
+	
+	$scope.addGoods = function () {
+		console.log(this.item);
+		cartServ.addGoods(this.item);
+	}
 }])
