2ac8477b70d77f3cfbc0fe8bba2d13428aff75f1
lzx <597930961@qq.com>
2017-03-25 10:40:43 +0800

    店铺详情页面倒计时功能以及筛选排序

--- shop/js/shop.js
+++ shop/js/shop.js
@@ -1,8 +1,21 @@
-app.controller('shopCtrl', ['$http', '$scope', '$stateParams','$state','collectServ',function ($http, $scope, $stateParams, $state,collectServ) {
+app.controller('shopCtrl', ['$http', '$scope', '$stateParams','$state','collectServ', '$interval',function ($http, $scope, $stateParams, $state,collectServ, $interval) {
 	//collectServ.Img().src="/shop/img/collect.png";
 	//collectServ.cnt()=1;
 	var itemStr = $stateParams.id;
 	$scope.item = JSON.parse(itemStr);
+	$scope.onlyTime = $scope.item.end_time - $scope.item.start_time
+	function addZero (num) {
+		return num > 10 ? num : '0' + num
+	}
+	function dateFormat () {
+		return addZero(parseInt($scope.onlyTime/100/60/60)) + '时' + addZero(parseInt($scope.onlyTime/(100*60)%60)) + '分' + parseInt($scope.onlyTime/100 %60) + '秒'
+	}
+	$scope.time = dateFormat($scope.onlyTime)
+	$interval(function () {
+		$scope.onlyTime -= 100
+		$scope.time = dateFormat($scope.onlyTime)
+	}, 1000)
+	console.log($scope.onlyTime/100%60)
 	$scope.showGoods = function () {
 		console.log(this)
 		$state.go('goods', {id: JSON.stringify(this.items)})
@@ -14,6 +27,13 @@ app.controller('shopCtrl', ['$http', '$scope', '$stateParams','$state','collectS
 		console.log(collectServ.collectArr())
 		collectServ.collectItemFn(collectItem);
 	}
+	$scope.changeAttr = function (attr) {
+		$scope.attribute = attr
+	}
+	$scope.filterAttr = function (num) {
+		$scope.num = num
+		$scope.attribute = 'oprice'
+	}	 
 	
 }])
 
