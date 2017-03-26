fcb2767283ee3c0fc5e0e25cd5e8e7374099a3b4
lzx <597930961@qq.com>
2017-03-26 15:59:16 +0800

    删除无用代码

--- filter/js/filter.js
+++ filter/js/filter.js
@@ -1,10 +1,8 @@
 app.controller('filterCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
 	$http.get('./data/global-beauty.json').then(function (data) {
-		console.log(data.data.list)
 		$scope.data = data.data.list;
 	})
 	$scope.showGoods = function () {
-		console.log(this.item)
 		$state.go('goods', {id : JSON.stringify(this.item)})
 	} 
 }])
